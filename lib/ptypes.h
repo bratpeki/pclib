8035a66ea8f666b2f67cbd76a03dfdaf55898a6a
bratpeki <katic.peco@gmail.com>
2023-12-31 17:32:20 +0100

    Added FUTURE, improved GUIDELINES and types

--- lib/ptypes.h
+++ lib/ptypes.h
@@ -5,22 +5,14 @@
  * pytpes.h - The definitions of various datatypes
  */
 
-/* === BYTE TYPES === */
-
 /* A signed byte */ typedef signed char p_sbyte;
 /* An unsigned byte */ typedef unsigned char p_ubyte;
 
-/* === INTEGER NUMBER TYPES === */
-
 /* A signed integer number */ typedef signed int p_sint;
 /* A signed long integer number */ typedef long signed int p_lsint;
-/* A signed long long integer number */ typedef long long signed int p_llsint;
 
 /* An unsigned integer number */ typedef unsigned int p_uint;
 /* An unsigned long integer number */ typedef long unsigned int p_luint;
-/* An unsigned long long integer number */ typedef long long unsigned int p_lluint;
-
-/* === FLOATING POINT NUMBER TYPES === */
 
 /* A signed floating point number */ typedef float p_flt;
 /* A signed double-precision floating point number */ typedef double p_dbl;
@@ -28,14 +20,11 @@
 /* A signed double-precision long floating point number */ typedef long double p_llflt;
 /* A signed double-precision long floating point number */ typedef long double p_ldbl;
 
-/* === LONELY TYPES === */
-
 /* A void pointer */ typedef void* p_vptr;
 
 /* An ASCII character */ typedef unsigned char p_chr;
 /* A C-style string */ typedef char* p_str;
 
-/* TODO: bools */
-/* TODO: remove the "TYPES" headers, level the code */
+/* A Boolean value */ typedef enum { p_false, p_true } p_bool;
 
 #endif
