ce116044d57d078fb86f391dfb6ef35d60be6c62
bratpeki <katic.peco@gmail.com>
2024-01-02 00:26:50 +0100

    debug commented

--- lib/pdebug.h
+++ lib/pdebug.h
@@ -1,13 +1,26 @@
 #ifndef PCLIB_DEBUG
 #define PCLIB_DEBUG
 
+/*
+ * pdebug.h - Variables and functions for debug output
+ *
+ * "Debug output" refers to the output of debugging messages to STDOUT.
+ */
+
 #include "./ptypes.h"
 
 #include <stdio.h>
 
-p_bool pDebugToggle = p_false;
+/*
+ * The Boolean value which enables debug output
+ * True by default
+ */
+p_bool pDebugToggle = p_true;
 
+/* If debugging is enabled, print the debugging message to STDOUT */
 #define pDebugPrintf(x) if(pDebugToggle)printf(x)
+
+/* Set whether or not debug output is on */
 #define pDebugSet(x) pDebugToggle = x
 
 #endif
